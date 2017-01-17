76e7c57f51a6bfba2cd9d60c26fcd4ca35b56201
Viren Bhagat <virenb@gmail.com>
2017-01-17 14:23:59 -0500

    Updated js file with ajax call and button functionality

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+
+    var quote;
+    var author;
+
     function getNewQuote() {
         $.ajax({
             url: 'http://api.forismatic.com/api/1.0/',
@@ -10,9 +14,26 @@ $(document).ready(function() {
             lang: 'en'
         },
         success: function(response) {
-            console.log(response.quoteText);
+            quote = response.quoteText;
+            author = response.quoteAuthor;
+            $('#quote').text(quote);
+            if (author) {
+                    $('#author').text('said by ' + author);
+            } else {
+                $('#author').text('- unknown');
+            }
             }
         });
     }
     getNewQuote();
+
+    $('.get-quote').on('click', function(e) {
+        e.preventDefault();
+        getNewQuote();
+    });
+
+    $('#tweet').on('click', function(e) {
+        e.preventDefault();
+        window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent(quote));
+    });
 });
